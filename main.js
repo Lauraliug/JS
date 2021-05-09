fd276ef818128361b407b7bea3846957e9f45761
Laura <lauraliugailaite@gmail.com>
2021-05-09 20:49:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*function getData(data) {
+  let HTML = "";
+  let middleBody = document.querySelector(".middle");
+
+  if (Array.isArray(data)) {
+    for (let i = 0; i < data.length; i++) {
+      HTML += getList(data[i]);
+    }
+    return (middleBody.innerHTML = HTML);
+  }
+}
+function getList(list) {
+
+  let HTML = '';
+  return HTML;
+}
+
+getData(feed);*/
+
+//--------------------------------------
